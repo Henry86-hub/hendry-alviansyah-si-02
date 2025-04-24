1029474ff6d17163a096a7de273ecc74bef28a83
Henry86-hub <henryalviansyah86@gmail.com>
2025-04-24 15:54:13 +0700

    Add files via upload

---
+++ Nilai Mahasiswa/tugasc++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int nilai_mahasiswa;
+cout<<"Masukan nilai mahasiswa:";
+cin>>nilai_mahasiswa;
+if(nilai_mahasiswa >= 60)
+cout<<"Lulus"<<endl; else
+cout<<"Tidak Lulus"<<endl;
+return 0;
+}
